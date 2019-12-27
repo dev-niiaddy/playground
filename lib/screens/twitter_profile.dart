50d976a06e342836436f107008ff4ce210f97f05
Addy Godwin Nii <dev.niiaddy@gmail.com>
2019-12-27 12:13:20 +0000

    added comments

--- lib/screens/twitter_profile.dart
+++ lib/screens/twitter_profile.dart
@@ -2,7 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:playground/util/constants.dart';
 
+/// The [ScrollController] [offset] at which the animation of the
+/// profile photo [Container] size and [Stack] position is stopped.
 const double ANIM_END_OFFSET = 65.0;
+
+const double PROFILE_DIM_LARGE = 90.0;
+
+const double PROFILE_DIM_SMALL = 50.0;
+
+const double POSITION_TOP_START = 104.0;
+
+const double POSITION_LEFT_START = 10.0;
 
 class TwitterProfile extends StatefulWidget {
   @override
@@ -11,9 +21,15 @@ class TwitterProfile extends StatefulWidget {
 
 class _TwitterProfileState extends State<TwitterProfile>
     with TickerProviderStateMixin {
+  ///
+  /// Tab controller to control tabs
   TabController _tabController;
+
+  /// Controller for [CustomScrollView] to detect
+  /// user scroll events
   ScrollController _scrollController;
 
+  /// List of tab String titles
   List<String> tabs = [
     'Tweet',
     'Tweets & Replies',
@@ -30,6 +46,8 @@ class _TwitterProfileState extends State<TwitterProfile>
       length: tabs.length,
     );
 
+    /// Initialize Scroll Controller and add
+    /// a listener
     _scrollController = ScrollController()
       ..addListener(() {
         setState(() {});
@@ -166,6 +184,10 @@ class _TwitterProfileState extends State<TwitterProfile>
                   ),
                 ),
               ),
+
+              /// Widget to create a stick-to-top effect for its delegate
+              /// when [pinned] is set to true. [TabBar] shouldn't scroll out of
+              /// view when the remaining content scrolls off.
               SliverPersistentHeader(
                 pinned: true,
                 delegate: StickyTabBarDelegate(
@@ -180,6 +202,9 @@ class _TwitterProfileState extends State<TwitterProfile>
                   ),
                 ),
               ),
+
+              /// A very long [Container] in a [Sliver] to demonstrate
+              /// stick-to-top effect of the [TabBar]
               SliverToBoxAdapter(
                 child: Container(
                   height: 1000,
@@ -194,67 +219,102 @@ class _TwitterProfileState extends State<TwitterProfile>
     );
   }
 
+  /// Return a profile photo [Container] of appropriate dimension depending
+  /// on which position we are in the [Stack] and the [ScrollController] [offset].
+  /// An insignificant [SizedBox] of zero height and zero width is returned
   Widget buildProfilePhoto(StackPosition pos) {
-    if (pos == StackPosition.TOP) {
-      double animEndOffset = ANIM_END_OFFSET;
+    double animEndOffset = ANIM_END_OFFSET;
 
-      double multiplier = 0.0;
-      double proDim = 90;
-      double posLeft = 10;
-      double posTop = 104;
+    double multiplier = 0.0;
 
-      if (_scrollController.hasClients) {
-        double scrollOffset = _scrollController.offset;
+    /// Initial state values for the
+    /// [proDim] profile dimension
+    /// [posLeft] position left
+    /// [posTop] position top
+    double proDim = PROFILE_DIM_LARGE;
+    double posLeft = POSITION_LEFT_START;
+    double posTop = POSITION_TOP_START;
 
-        multiplier = (scrollOffset / animEndOffset).clamp(0.0, 1.0);
+    if (_scrollController.hasClients) {
+      // How far has the user scrolled
+      double scrollOffset = _scrollController.offset;
 
-        proDim -= (40 * multiplier);
-        posTop += (40 * multiplier) - scrollOffset;
-        posLeft += (20 * multiplier);
-      }
+      /// A ratio of how close user has scrolled to the end state
+      /// The ratio cannot be negative or greater than one
+      multiplier = (scrollOffset / animEndOffset).clamp(0.0, 1.0);
 
-      return Positioned(
-          top: posTop,
-          // height: 90,
-          left: posLeft,
-          // width: 90,
-          child: (proDim != 50)
-              ? Container(
-                  height: proDim,
-                  width: proDim,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                      image: AssetImage('assets/images/profile_lady.jpg'),
-                      fit: BoxFit.cover,
-                    ),
-                    border: Border.all(
-                      color: Colors.blue,
-                      width: 3.0,
-                    ),
-                  ),
-                )
-              : SizedBox(
-                  height: 0,
-                  width: 0,
-                ));
+      /// Final [proDim] is 50. A fraction of 40 is computed to
+      /// subtract from the initial 90 to get close to 50, as the ratio
+      /// increases or gets closer to one.
+      proDim -= (40 * multiplier);
+
+      /// Final [posTop] is 144. A fraction of 40 is computed to
+      /// add to the initial 104 to get close to 144, as the ratio
+      /// increases or gets closer to one. The offset is subtracted to
+      /// keep the profile photo in sync with the scroll.
+      posTop += (40 * multiplier) - scrollOffset;
+
+      /// Final [posLef] is 30. A fraction of 20 is computed to
+      /// add to the initial 10 to get close to 30, as the ratio
+      /// increases or gets closer to one.
+      posLeft += (20 * multiplier);
     }
 
-    return Container(
-      height: 50,
-      width: 50,
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        image: DecorationImage(
-          image: AssetImage('assets/images/profile_lady.jpg'),
-          fit: BoxFit.cover,
-        ),
-        border: Border.all(
-          color: Colors.blue,
-          width: 3.0,
-        ),
-      ),
-    );
+    ///Check if the required profile photo [Container] is supposed to be at
+    ///the top of the Stack.
+    if (pos == StackPosition.TOP) {
+      return Positioned(
+        top: posTop,
+        // height: 90,
+        left: posLeft,
+        // width: 90,
+        child: (proDim != 50)
+            ? Container(
+                height: proDim,
+                width: proDim,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/profile_lady.jpg'),
+                    fit: BoxFit.cover,
+                  ),
+                  border: Border.all(
+                    color: Colors.blue,
+                    width: 3.0,
+                  ),
+                ),
+              )
+            : SizedBox(
+                height: 0,
+                width: 0,
+              ),
+      );
+    }
+
+    /// If the profile photo dimension [proDim] is
+    /// equal to [PROFILE_DIM_SMALL] then show image
+    /// in underlying [Container] else show a [SizedBox]
+    /// of equivalent height and width
+    return (proDim == PROFILE_DIM_SMALL)
+        ? Container(
+            height: PROFILE_DIM_SMALL,
+            width: PROFILE_DIM_SMALL,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image: AssetImage('assets/images/profile_lady.jpg'),
+                fit: BoxFit.cover,
+              ),
+              border: Border.all(
+                color: Colors.blue,
+                width: 3.0,
+              ),
+            ),
+          )
+        : SizedBox(
+            height: PROFILE_DIM_SMALL,
+            width: PROFILE_DIM_SMALL,
+          );
   }
 
   void onEditProfile() {}
